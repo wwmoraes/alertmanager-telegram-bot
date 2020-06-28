e44d396bfd9f0f997ac48a3044e5827c3e47d08d
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2020-06-28 12:37:52 +0200

    test: cleanup unused manual fixtures

--- src/__fixtures__/mockChat.ts
+++
@@ -1,6 +0,0 @@
-import {Chat} from "telegraf/typings/telegram-types";
-
-export const mockChat: Chat = {
-  id: 1,
-  type: "private"
-};
--- src/__fixtures__/mockUpdate.ts
+++
@@ -1,5 +0,0 @@
-import {Update} from "telegraf/typings/telegram-types";
-
-export const mockUpdate: Update = {
-  update_id: 1
-};
