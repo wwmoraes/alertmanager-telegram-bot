4bfa95abb056a2105172a054cfeeee9d54261d98
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2020-06-28 10:50:49 +0200

    test: use fixtures to test helpCommand

--- src/bot/helpCommand.spec.ts
+++ src/bot/helpCommand.spec.ts
@@ -1,19 +1,6 @@
 /* eslint-disable no-undefined */
 /* eslint-disable @typescript-eslint/no-empty-function */
 
-import {BotContext} from "./BotContext";
-import {mock} from "jest-mock-extended";
-import {Message} from "telegraf/typings/telegram-types";
-
-const botContext = mock<BotContext>();
-const mockMessage = mock<Message>();
-
-botContext.reply.mockImplementation(() =>
-  Promise.resolve(mockMessage));
-
-// const next = jest.fn(() =>
-//   Promise.resolve());
-
 beforeAll(() => {
   jest.spyOn(console, "warn").mockImplementation(() => {});
   jest.spyOn(console, "info").mockImplementation(() => {});
@@ -30,8 +17,10 @@ beforeEach(() => {
 it("should greet back", async () => {
   const helpCommand = await (await import("./helpCommand")).helpCommand;
 
-  await helpCommand(botContext);
+  const mockBotContextValid = await (await import("./__fixtures__/mockBotContext")).mockBotContextValid;
 
-  expect(botContext.reply).toHaveBeenCalled();
-  expect(botContext.reply).toHaveBeenCalledWith("Send me a sticker", undefined);
+  await helpCommand(mockBotContextValid);
+
+  expect(mockBotContextValid.reply).toHaveBeenCalled();
+  expect(mockBotContextValid.reply).toHaveBeenCalledWith("Send me a sticker", undefined);
 });
