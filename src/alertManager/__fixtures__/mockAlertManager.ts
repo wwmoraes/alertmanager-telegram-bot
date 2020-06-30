eae203270354bd1e891d0d959259b23148d65892
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2020-06-30 21:29:47 +0200

    test: fix memdown encoding

--- src/alertManager/__fixtures__/mockAlertManager.ts
+++ src/alertManager/__fixtures__/mockAlertManager.ts
@@ -4,13 +4,20 @@
  */
 
 import levelup from "levelup";
+import encode from "encoding-down";
 import memdown from "memdown";
 import {AlertManager} from "../AlertManager";
 import type {IAlert} from "../IAlert";
 
 export const alertManagerInstance = new AlertManager(
-  levelup(memdown<string, string>()),
-  levelup(memdown<string, IAlert>())
+  levelup(encode(memdown<string, string>(), {
+    valueEncoding: "string",
+    keyEncoding: "string"
+  })),
+  levelup(encode(memdown<string, IAlert>(), {
+    valueEncoding: "string",
+    keyEncoding: "json"
+  }))
 );
 
 export const addUserChatSpy = jest.spyOn(alertManagerInstance, "addUserChat");
