aacf17bba752f6178e5faab2238d9750dbb00b3a
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2020-07-03 23:18:48 +0200

    test: mock alertmanager with memdown

---
+++ src/alertManager/__mocks__/AlertManager.ts
@@ -0,0 +1,21 @@
+import levelup from "levelup";
+import encode from "encoding-down";
+import memdown from "memdown";
+import type {IAlert} from "../IAlert";
+
+const AlertManagerActual = jest.requireActual("../AlertManager").AlertManager;
+
+export class AlertManager extends AlertManagerActual {
+  constructor () {
+    super(
+      levelup(encode(memdown<string, string>(), {
+        valueEncoding: "string",
+        keyEncoding: "string"
+      })),
+      levelup(encode(memdown<string, IAlert>(), {
+        valueEncoding: "string",
+        keyEncoding: "json"
+      }))
+    );
+  }
+}
