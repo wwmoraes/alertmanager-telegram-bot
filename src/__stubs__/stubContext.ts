cb2cbf45400aedb7134266aaa060b1b664900673
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2020-07-06 13:38:17 +0200

    test: stub telegram calls instead of a full shallow mock

--- src/__stubs__/stubContext.ts
+++ src/__stubs__/stubContext.ts
@@ -9,18 +9,32 @@ import {stubUserTest} from "./stubUser";
 import {stubChat} from "./stubChat";
 import {stubMessageText} from "./stubMessage";
 import {stubStickerSet} from "./stubStickerSet";
+import {ExtraEditMessage, Message} from "telegraf/typings/telegram-types";
 
 export const stubContext = mock<Context>({
   from: stubUserTest,
   chat: stubChat,
   reply: jest.fn(() =>
     Promise.resolve(stubMessageText)),
-  telegram: mock<Telegram>({
-    getStickerSet: jest.fn((_setName) =>
-      Promise.resolve(stubStickerSet))
+  telegram: new Telegram("TELEGRAM_TOKEN", {
+    apiRoot: "https://api.telegram.localhost",
+    webhookReply: false
   }),
   message: stubMessageText
 });
+
+jest.spyOn(stubContext.telegram, "getStickerSet").mockImplementation((_setName) =>
+  Promise.resolve(stubStickerSet));
+jest.spyOn(stubContext.telegram, "sendMessage").mockImplementation((chatId: string|number, text: string, extra?: ExtraEditMessage) =>
+  Promise.resolve(<Message>{
+    ...stubMessageText,
+    message_id: extra?.reply_to_message_id || stubMessageText.message_id,
+    text,
+    chat: {
+      ...stubChat,
+      id: chatId
+    }
+  }));
 
 export default {
   stubContext
