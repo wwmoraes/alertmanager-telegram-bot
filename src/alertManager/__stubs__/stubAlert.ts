880837b85384e4073660f2b58a56f6c0ea7d928a
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2020-07-07 18:08:51 +0200

    test: alerts for secondary updates

--- src/alertManager/__stubs__/stubAlert.ts
+++ src/alertManager/__stubs__/stubAlert.ts
@@ -4,7 +4,14 @@
  */
 
 import {Alert} from "../Alert";
-import {stubIUpdateAlertFiring, stubIUpdateAlertResolved} from "./stubIUpdateAlert";
+import {
+  stubIUpdateAlertFiring,
+  stubIUpdateAlertResolved,
+  stubIUpdateAlertFiring2,
+  stubIUpdateAlertResolved2
+} from "./stubIUpdateAlert";
 
 export const stubAlertFiring = Alert.from(stubIUpdateAlertFiring);
 export const stubAlertResolved = Alert.from(stubIUpdateAlertResolved);
+export const stubAlertFiring2 = Alert.from(stubIUpdateAlertFiring2);
+export const stubAlertResolved2 = Alert.from(stubIUpdateAlertResolved2);
