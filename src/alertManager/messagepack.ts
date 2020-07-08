6a4cc154cacdfb0a4a12853ec161f374dd8e04dc
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2020-07-08 20:40:40 +0200

    docs: alertmanager messagepack

--- src/alertManager/messagepack.ts
+++ src/alertManager/messagepack.ts
@@ -5,9 +5,22 @@
 
 import {decode, encode} from "messagepack";
 
+/**
+ * encodes an object into a messagepack string
+ *
+ * @param {Object.<string, any>} obj object to encode
+ * @returns {string} encoded string
+ */
 export const encodeToString = (obj: Record<string, unknown>): string =>
   String.fromCharCode(...Array.from(encode(obj)));
 
+/**
+ * decodes a messagepack string into an object
+ *
+ * @template T
+ * @param {string} str encoded string
+ * @returns {T} object with decoded data
+ */
 export const decodeFromString = <T>(str: string): T =>
   decode<T>(new Uint8Array(str.split("").map((char) =>
     char.charCodeAt(0))).buffer);
