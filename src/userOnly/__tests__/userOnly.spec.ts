9074d6b131eef8d2c0c9a2023feb28f11cadedb3
William Artero <williamwmoraes@gmail.com>
2020-10-27 09:01:19 +0100

    lint: fix IUserOnlyContext usage

--- src/userOnly/__tests__/userOnly.spec.ts
+++ src/userOnly/__tests__/userOnly.spec.ts
@@ -4,13 +4,13 @@
  */
 
 import {userOnlyMiddleware} from "..";
-import IContext from "../typings/IUserOnlyContext";
+import type { IUserOnlyContext } from "../typings/IUserOnlyContext";
 
 it("should passthrough on unknown update type", () => {
   const next = jest.fn(() =>
     Promise.resolve());
 
-  expect(userOnlyMiddleware({} as IContext, next)).resolves.toBeUndefined();
+  expect(userOnlyMiddleware({} as IUserOnlyContext, next)).resolves.toBeUndefined();
   expect(next).toBeCalledTimes(1);
 });
 
@@ -18,7 +18,7 @@ it("should drop request on known updates without from", () => {
   const next = jest.fn(() =>
     Promise.resolve());
 
-  expect(userOnlyMiddleware({updateType: "message"} as IContext, next)).resolves.toBeUndefined();
+  expect(userOnlyMiddleware({updateType: "message"} as IUserOnlyContext, next)).resolves.toBeUndefined();
   expect(next).toBeCalledTimes(0);
 });
 
@@ -30,7 +30,7 @@ it("should drop request from non-admin", () => {
     updateType: "message",
     from: {id: 2},
     userIds: ["1"]
-  } as IContext, next)).resolves.toBeUndefined();
+  } as IUserOnlyContext, next)).resolves.toBeUndefined();
   expect(next).toBeCalledTimes(0);
 });
 
@@ -42,6 +42,6 @@ it("should forward admin requests", () => {
     updateType: "message",
     from: {id: 1},
     userIds: ["1"]
-  } as IContext, next)).resolves.toBeUndefined();
+  } as IUserOnlyContext, next)).resolves.toBeUndefined();
   expect(next).toBeCalledTimes(1);
 });
